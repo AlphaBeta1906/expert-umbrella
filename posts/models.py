167a24d8a57e77f32bb310f35e6f58c4997cb32b
alfarizi <farizi1906@gmail.com>
2023-11-28 18:55:28 +0700

    exclude some field from revision

--- posts/models.py
+++ posts/models.py
@@ -47,7 +47,7 @@ class GroupPost(models.Model):
         group = GroupPost.objects.filter(id=self.id).first()
         return Post.objects.filter(is_draft=False, group=self.id).count()
 
-@reversion.register()
+@reversion.register(exclude=("likes","chapter_id","is_hide"))
 class Post(models.Model):
     title = models.CharField(max_length=50, null=False, blank=False)
     content = models.TextField(
